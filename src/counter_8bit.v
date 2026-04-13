91d0697d6501ccd6fed47b9e738827988d52a090
lyszt <kalliddel@gmail.com>
2026-04-13 00:37:15 -0300

    fix: stops the program counter while writing

--- src/counter_8bit.v
+++ src/counter_8bit.v
@@ -2,12 +2,15 @@ module counter_8bit (
     input clk,
     input [7:0] num,
     input load,
+    input reset,
     output reg [7:0] count
 );
 
     always @(posedge clk) begin 
-        if (load)
-            count <= num;
+        if (reset) 
+            count <= 8'b00000000;
+        else if (load)
+            count <= count;
         else
             count <= count + 1;
     end 
