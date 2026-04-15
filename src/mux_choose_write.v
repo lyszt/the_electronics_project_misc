bb6d727ec20541b1c9a5b6cf22fb3a128a33735a
lyszt <kalliddel@gmail.com>
2026-04-14 22:07:07 -0300

    wait up he's cooking smthn

---
+++ src/mux_choose_write.v
@@ -0,0 +1,20 @@
+module program_counter (
+    input clk,
+    input [7:0] data_in,
+    input reset,
+    output reg [7:0] data_out
+
+);
+    initial begin 
+        data_out = 8'b00000000;
+    end 
+    always @(posedge clk) begin 
+        if(reset) begin 
+            data_out <= 8'b00000000;
+        end
+        else begin 
+            data_out <= data_in + 1;
+        end
+    end 
+
+endmodule 
